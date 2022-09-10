eef01e371de72df29218b41f0cd0da1860a53f4b
Alex Souza <alex.webdevelloper@gmail.com>
2022-09-10 17:21:08 -0300

    Feat: Adds errors logging

--- lib/middlewares/errorHandler.ts
+++ lib/middlewares/errorHandler.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 function errorHandler(error: { type: string, message: string | number }, req: Request, res: Response, next: NextFunction){
 
+    console.log('error ------------------------------', error);
+
     if(error.type === 'not_found') return res.status(404).send(error.message);
     if(error.type === 'conflict') return res.status(422).send(error.message);
     if(error.type === 'unauthorized') return res.status(401).send(error.message);
